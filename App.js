d924cccc5edbfe9b09a3e528863f9c5af721c48a
Sk-Abdul-Rahim <86352451+Sk-Abdul-Rahim@users.noreply.github.com>
2021-12-19 17:59:34 +0500

    Add files via upload
    Khana Sab Lye Mobile App

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View,Text } from 'react-native';
+// import Button from './src/component/Button'
+import Navigation from './src/config/navigation/Navigation'
+import Login from './src/screen/Login'
+
+export default function App() {
+  return <Navigation/>
+  // (
+  //   <View style={styles.container}>
+  //     <Text>Hello World</Text>
+  //   </View>
+  // )
+}
